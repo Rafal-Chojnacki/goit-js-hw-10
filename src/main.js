d9086e4e54641dd56c30828d3a68cfea967a07d2
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-05-22 23:37:15 +0200

    Adding elemts(name, flag.svg)-li to country-list-ul

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
+import Notiflix from 'notiflix';
 import { fetchCountries } from './fetch';
-import { debounce } from 'lodash';
+import { debounce, functions } from 'lodash';
 
 const nameInput = document.getElementById('search-box');
-let countryList = [];
 
-nameInput.addEventListener('input', debounce(inputHandler, 300));
+nameInput.addEventListener('input', debounce(inputHandler, 1000));
 
 function inputHandler(e) {
-  const name = e.target.value;
+  const name = e.target.value.trim();
 
   fetchCountries(name)
     .then(countries => {
-      countryList = countries.map(
+      const countryList = countries.map(
         country =>
           new Country(
             country.name.official,
@@ -22,12 +22,26 @@ function inputHandler(e) {
             country.flags.alt
           )
       );
+      clearList();
+      clearCountryInfo();
+      if (countryList.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (countryList.length > 1) {
+        updateCountryList(countryList);
+        return;
+      }
 
-      console.log(countryList);
+      updateOneCountry(countryList[0]);
     })
     .catch(error => {
       console.error(error);
-      // obsłuż błąd żądania
+      Notiflix.Notify.warning('Oops, there is no country with that name');
+      clearList();
+      clearCountryInfo();
     });
 }
 
@@ -40,4 +54,72 @@ class Country {
     this.flagFile = flagFile;
     this.alt = alt;
   }
+}
+
+function updateCountryList(countryList) {
+  const ulCountry = document.querySelector('.country-list');
+  ulCountry.style.listStyleType = 'none';
+  for (const country of countryList) {
+    const ulCountry = document.querySelector('.country-list');
+    const countryName = country.name;
+    const flagURL = country.flagFile;
+    const flagAlt = country.alt;
+
+    // Create the <li> element
+    const liCountry = document.createElement('li');
+    liCountry.style.fontSize = 20;
+    liCountry.style.fontFamily = 'Arial, sans-serif';
+    liCountry.style.fontWeight = 'bold';
+    liCountry.style.padding = 2;
+
+    // Create the <img> element and set its attributes
+    const flagImage = document.createElement('img');
+    flagImage.setAttribute("width", "40");
+    flagImage.style.border = "1px solid black";
+    flagImage.style.margin = 2;
+    flagImage.src = flagURL;
+    flagImage.alt = flagAlt;
+
+    // Create the text node for the country name
+    const textNode = document.createTextNode(countryName);
+
+    // Append the <img> and text node to the <li> element
+    liCountry.appendChild(flagImage);
+    liCountry.appendChild(textNode);
+
+    // Append the <li> element to the <ul> element
+    ulCountry.appendChild(liCountry);
+  }
+}
+function clearList() {
+  const ulCountry = document.querySelector('.country-list');
+  ulCountry.innerHTML = '';
+}
+function clearCountryInfo() {
+  const countryInfo = document.querySelector('.country-info');
+  countryInfo.innerHTML = '';
+}
+function updateOneCountry(country) {
+  const ulCountry = document.querySelector('.country-list');
+  const countryName = country.name;
+  const flagURL = country.flagFile;
+  const flagAlt = country.alt;
+
+  // Create the <li> element
+  const liCountry = document.createElement('li');
+
+  // Create the <img> element and set its attributes
+  const flagImage = document.createElement('img');
+  flagImage.src = flagURL;
+  flagImage.alt = flagAlt;
+
+  // Create the text node for the country name
+  const textNode = document.createTextNode(countryName);
+
+  // Append the <img> and text node to the <li> element
+  liCountry.appendChild(flagImage);
+  liCountry.appendChild(textNode);
+
+  // Append the <li> element to the <ul> element
+  ulCountry.appendChild(liCountry);
 }
